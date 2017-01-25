52ecbdf81910b7df0342e07f70766f2ce0a91bf0
Rien <rien@balancingrock.nl>
2017-01-25 09:57:21 +0100

    Moved c-code out, included COpenSsl as dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SecureSockets",
     dependencies: [
-        .Package(url: "https://github.com/Swiftrien/SwifterSockets", "0.9.8")
+        .Package(url: "https://github.com/Balancingrock/SwifterSockets", "0.9.8"),
+        .Package(url: "https://github.com/Balancingrock/COpenSsl", "0.1.0")
     ]
 )
