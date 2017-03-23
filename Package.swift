cb9ddc071ef0243e319b5ab87be3397196db185d
Balancingrock <github@balancingrock.nl>
2017-03-23 09:44:14 +0100

    Updated for compilation speed increase

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SecureSockets",
     dependencies: [
-        .Package(url: "https://github.com/Balancingrock/SwifterSockets", "0.9.15"),
+        .Package(url: "https://github.com/Balancingrock/SwifterSockets", "0.10.1"),
         .Package(url: "https://github.com/Balancingrock/COpenSsl", "0.1.0")
     ]
 )
